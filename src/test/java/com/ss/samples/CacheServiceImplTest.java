6c9a45a85c3fe07da8c0fa42a749359abb5cb072
iholovata <irina-golovataja@ukr.net>
2020-02-12 12:37:07 +0200

    the refactoring of test-class was done

--- src/test/java/com/ss/samples/CacheServiceImplTest.java
+++ src/test/java/com/ss/samples/CacheServiceImplTest.java
@@ -3,7 +3,6 @@ package com.ss.samples;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
@@ -15,16 +14,10 @@ import static org.junit.Assert.assertEquals;
 public class CacheServiceImplTest {
 
     CacheServiceImpl cacheService;
-    CacheServiceImpl cacheServiceSpy;
-
-    private static void accept(String s) throws Exception {
-        throw new Exception("Element with key: " + s + " already exist!");
-    }
 
     @Before
     public void before() {
         cacheService = new CacheServiceImpl();
-        cacheServiceSpy = Mockito.spy(cacheService);
         CacheServiceImpl.instance = new HashMap<>();
     }
 
@@ -34,12 +27,9 @@ public class CacheServiceImplTest {
     }
 
     @Test
-    public void get() throws Exception {
+    public void get() {
         User expectedObject = new User(100500, "User");
         CacheServiceImpl.instance.put(String.valueOf(expectedObject.getId()), expectedObject);
-        Function<String, Object> sourceFunction = s -> expectedObject;
-
-        setValueToPrivateField(cacheServiceSpy, "sourceFunction", sourceFunction);
 
         assertEquals(expectedObject, cacheService.get(String.valueOf(expectedObject.getId())));
     }
@@ -48,7 +38,7 @@ public class CacheServiceImplTest {
     public void getIfNotExistInRealValue() throws Exception {
         User user = new User(100500, "User");
         Function<String, Object> sourceFunction = s -> null;
-        setValueToPrivateField(cacheServiceSpy, "sourceFunction", sourceFunction);
+        setValueToPrivateField(cacheService, "sourceFunction", sourceFunction);
 
         cacheService.get(String.valueOf(user.getId()));
     }
@@ -58,37 +48,46 @@ public class CacheServiceImplTest {
         User expectedObject = new User(100500, "User");
         Function<String, Object> sourceFunction = s -> expectedObject;
 
-        setValueToPrivateField(cacheServiceSpy, "sourceFunction", sourceFunction);
+        setValueToPrivateField(cacheService, "sourceFunction", sourceFunction);
 
-        assertEquals(expectedObject, cacheServiceSpy.get(String.valueOf(expectedObject.getId())));
+        assertEquals(expectedObject, cacheService.get(String.valueOf(expectedObject.getId())));
     }
 
     @Test
     public void put() throws Exception {
         Consumer<String> handlerToMock = System.out::println;
-        setValueToPrivateField(cacheServiceSpy, "handler", handlerToMock);
+        setValueToPrivateField(cacheService, "handler", handlerToMock);
 
         User user = new User(100500, "User");
-        cacheServiceSpy.put(String.valueOf(user.getId()), user);
+        cacheService.put(String.valueOf(user.getId()), user);
         assertEquals(user, CacheServiceImpl.instance.get(String.valueOf(user.getId())));
     }
 
     @Test
     public void putIfExist() throws Exception {
         Consumer<String> handlerToMock = System.out::println;
-        setValueToPrivateField(cacheServiceSpy, "handler", handlerToMock);
+        setValueToPrivateField(cacheService, "handler", handlerToMock);
 
         User user = new User(100500, "User");
-        User userTwo = new User(100500, "UserTwo");
+        User userTwo = new User(100500, "User Two with the same id");
 
-        cacheServiceSpy.put(String.valueOf(user.getId()), user);
-        cacheServiceSpy.put(String.valueOf(user.getId()), user);
+        cacheService.put(String.valueOf(user.getId()), user);
+        cacheService.put(String.valueOf(userTwo.getId()), userTwo);
         User actualUser = (User) CacheServiceImpl.instance.get(String.valueOf(user.getId()));
-        assertEquals(user, actualUser);
+        assertEquals(userTwo.getName(), actualUser.getName());
+    }
+
+    private void setValueToPrivateField(
+            Object targetObject,
+            String fieldName,
+            Object value
+    ) throws NoSuchFieldException, IllegalAccessException {
+        Field sourceFunctionField = CacheServiceImpl.class.getDeclaredField(fieldName);
+        sourceFunctionField.setAccessible(true);
+        sourceFunctionField.set(targetObject, value);
     }
 
     public static class User {
-
         private long id;
         private String name;
 
@@ -101,25 +100,15 @@ public class CacheServiceImplTest {
             return id;
         }
 
-        public void setId(long id) {
-            this.id = id;
-        }
-
         public String getName() {
             return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
         }
 
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (!(o instanceof User)) return false;
-
             User user = (User) o;
-
             if (getId() != user.getId()) return false;
             return getName() != null ? getName().equals(user.getName()) : user.getName() == null;
         }
@@ -130,12 +119,5 @@ public class CacheServiceImplTest {
             result = 31 * result + (getName() != null ? getName().hashCode() : 0);
             return result;
         }
-
-    }
-
-    private void setValueToPrivateField(Object targetObject, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
-        Field sourceFunctionField = CacheServiceImpl.class.getDeclaredField(fieldName);
-        sourceFunctionField.setAccessible(true);
-        sourceFunctionField.set(targetObject, value);
     }
 }
