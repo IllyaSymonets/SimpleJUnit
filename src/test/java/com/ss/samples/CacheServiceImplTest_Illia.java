54bcc9935990c986d92b74e132585c220f34ff0c
“Illia <Illia.Symonets@davita.com>
2020-02-12 15:47:05 +0200

    CacheServiceImplTest_Illia tests have been added

--- src/test/java/com/ss/samples/CacheServiceImplTest_Illia.java
+++ src/test/java/com/ss/samples/CacheServiceImplTest_Illia.java
@@ -2,59 +2,28 @@ package com.ss.samples;
 
 import static java.lang.System.currentTimeMillis;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
-import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.mockito.internal.util.reflection.FieldSetter;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 
-@PrepareForTest(CacheServiceImpl.class)
 public class CacheServiceImplTest_Illia {
 
     CacheServiceImpl cacheService;
-    CacheServiceImpl cacheServiceSpy;
-    Function<String, Object> sourceFunctionTest =
-        Mockito.mock(Function.class);
-    Consumer<String> handlerTest =
-        Mockito.mock(Consumer.class);
-
+    Consumer<String> handlerMock;
+    Function<String, Object> sourceFunctionMock;
 
     @Before
     public void before() {
         cacheService = new CacheServiceImpl();
-        cacheServiceSpy = Mockito.spy(cacheService);
-        CacheServiceImpl.instance = new HashMap<>();
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void getRealValueRuntimeExceptionTest() {
-
-        cacheService.get("TEST-GET");
-        Mockito.verify(cacheServiceSpy).getRealValue("TEST-GET");
-    }
-
-    @Test
-    public void getRealValueTest() {
-
-        Object testObject = new Object();
-        Mockito.when(cacheServiceSpy.
-            getRealValue("TEST-GET")).thenReturn(testObject);
-
-        assertEquals(testObject, cacheServiceSpy.get("TEST-GET"));
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void handleKeyExistsTest() {
-
-        cacheService.put("TEST-PUT", currentTimeMillis());
-        cacheService.put("TEST-PUT", currentTimeMillis());
-        Mockito.verify(cacheServiceSpy).handleKeyExists("TEST-GET");
+        handlerMock = Mockito.mock(Consumer.class);
+        sourceFunctionMock = Mockito.mock(Function.class);
+        cacheService.setSourceFunction(sourceFunctionMock);
+        cacheService.setHandler(handlerMock);
+        cacheService.instance = new HashMap<>();
     }
 
     @Test
@@ -64,7 +33,7 @@ public class CacheServiceImplTest_Illia {
 
         CacheServiceImpl impl = prepareDataForTest("TEST-PUT", testValue);
 
-        assertEquals(testValue, CacheServiceImpl.instance.get("TEST-PUT"));
+        assertEquals(testValue, impl.instance.get("TEST-PUT"));
     }
 
     @Test
@@ -77,33 +46,43 @@ public class CacheServiceImplTest_Illia {
         assertEquals(testValue, impl.get("TEST-GET"));
     }
 
-    @Test
-    public void sourceFunctionTest() throws Exception {
+    @Test(expected = RuntimeException.class)
+    public void putRuntimeExceptionTest() {
 
-        FieldSetter.setField(cacheService,
-            cacheService.getClass().
-                getDeclaredField("sourceFunction"),
-            sourceFunctionTest);
-        Object testObject = new Object();
+        long testValue = currentTimeMillis();
+        cacheService.setHandler(null);
 
-        Mockito.when(sourceFunctionTest.apply("TEST-GET")).thenReturn(testObject);
-
-        assertEquals(testObject, cacheService.get("TEST-GET"));
+        cacheService.put("TEST-PUT", testValue);
+        cacheService.put("TEST-PUT", testValue);
     }
 
     @Test
-    public void handlerTest() throws Exception {
+    public void putIsHandlerExecutesTest() {
 
-        FieldSetter.setField(cacheService,
-            cacheService.getClass().
-                getDeclaredField("handler"),
-            handlerTest);
+        long testValue = currentTimeMillis();
 
-        cacheService.put("TEST-PUT", 1);
-        cacheService.put("TEST-PUT", 2);
+        cacheService.put("TEST-PUT", testValue);
+        cacheService.put("TEST-PUT", testValue);
 
-        assertTrue(Objects.nonNull(handlerTest)
-            && cacheService.get("TEST-PUT").equals(2));
+        Mockito.verify(handlerMock).accept("TEST-PUT");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void getRuntimeExceptionTest() {
+
+        cacheService.get("TEST-GET");
+
+    }
+
+    @Test
+    public void getSourceFunctionWorksCorrectlyTest() {
+
+        long testValue=System.currentTimeMillis();
+        Mockito.when(sourceFunctionMock.apply("TEST-GET"))
+            .thenReturn(testValue);
+
+        assertEquals(testValue, cacheService.get("TEST-GET"));
+
     }
 
     private CacheServiceImpl prepareDataForTest(String key, long testValue) {
