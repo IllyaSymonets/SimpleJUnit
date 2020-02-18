3b35901a56df1753a3ba9f1661ce264c4037b882
“Illia <Illia.Symonets@davita.com>
2020-02-18 11:49:15 +0200

    CacheServiceChildImpl updateStatistic has been tested

--- src/test/java/com/ss/samples/CacheServiceChildImplTest.java
+++ src/test/java/com/ss/samples/CacheServiceChildImplTest.java
@@ -9,11 +9,13 @@ import org.mockito.Mockito;
 public class CacheServiceChildImplTest {
 
     private CacheServiceChildImpl testCache;
+    private CacheServiceChildImpl testCacheSpy;
 
     @Before
     public void prepareDataForTest() {
         testCache = new CacheServiceChildImpl();
         testCache.setMaxCapacity(100);
+        testCacheSpy = Mockito.spy(testCache);
     }
 
     @Test
@@ -23,7 +25,8 @@ public class CacheServiceChildImplTest {
     @Test
     public void putWithCleaningCorrect() {
         for (int i = 0; i < 101; i++) {
-            CacheServiceChildImpl.CachedEntityChild cachedEntity = new CacheServiceChildImpl.CachedEntityChild(String.valueOf(i), new Object());
+            CacheServiceChildImpl.CachedEntityChild cachedEntity = new CacheServiceChildImpl.CachedEntityChild(
+                String.valueOf(i), new Object());
             int number = (int) (Math.random() * 300 + 10);
             cachedEntity.setCounterOfUsage(number);
             testCache.put(String.valueOf(i), cachedEntity);
@@ -32,7 +35,10 @@ public class CacheServiceChildImplTest {
     }
 
     @Test
-    public void updateStatistic(){
-
+    public void updateStatistic() throws NoSuchFieldException {
+        testCacheSpy.put("TEST-GET", new Object());
+        testCacheSpy.get("TEST-GET");
+        Mockito.verify(testCacheSpy).updateStatistic(Mockito.any(
+            CacheServiceChildImpl.CachedEntityChild.class));
     }
 }
